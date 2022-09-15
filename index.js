c7adc35b1881790584842078f2f3f4a6a46c0aa0
Zeronicc <zeronic4465@gmail.com>
2022-09-14 19:10:38 -0500

    added HTML template along with functional ui

--- index.js
+++ index.js
@@ -1,54 +1,66 @@
+const currentResult = document.getElementById('result');
+const currentPoints = document.getElementById('points');
+const outcome = document.querySelector('.textResult');
+
+const player = document.querySelector('.buttons');
+player.addEventListener('click', e => playRound());
+
+
+const rockButton = document.querySelector('#rock');
+rockButton.addEventListener("click", (e) => choose = 'Rock');
+
+const paperButton = document.querySelector('#paper');
+paperButton.addEventListener('click', (e) => choose = 'Paper')
+
+const scissorsButton = document.querySelector('#scissors');
+scissorsButton.addEventListener("click", (e) => choose = 'Scissors');
+
 let points = 0;
-let result = 0;
+let playerPoints = 0;
+let choose = '';
+
+
 function playRound(playerSelection, computerSelection){ 
-    if(playerSelection == 'Rock' && computerSelection == 'Paper') {
-        ++points;
-        return "You Lose! Paper beats Rock " ;    
-   } else if(playerSelection == 'Paper' && computerSelection == 'Scissors'){
-        ++points;
-        return "You lost! Scissors cuts Paper ";
-   } else if (playerSelection == 'Scissors' && computerSelection == 'Rock'){
-        ++points;
-        return "You Lose! Rock crushes Scissors ";
-   } else if(playerSelection == computerSelection){
-        return "It's a tie! Try again ";
-   } else if(!(playerSelection == 'Rock' || playerSelection == 'Paper' || playerSelection == 'Scissors')){
-        return "Type your response correctly!"
+    const computerChoice = getComputerChoice();
+    const playerChoice= getPlayerChoice();
+    if(playerChoice == 'Rock' && computerChoice == 'Paper') {
+        currentPoints.textContent = ++points;
+        outcome.textContent = "You lose!" ;    
+   } else if(playerChoice == 'Paper' && computerChoice == 'Scissors'){
+        currentPoints.textContent = ++points;
+        outcome.textContent = "You lose!";
+   } else if (playerChoice == 'Scissors' && computerChoice == 'Rock'){
+        currentPoints.textContent = ++points;
+        outcome.textContent = "You lose!";
+   } else if(playerChoice == computerChoice){
+        outcome.textContent = "It's a tie!";
+   } else if(!(playerChoice == 'Rock' || playerChoice == 'Paper' || playerChoice == 'Scissors')){
+        outcome.textContent = "ERROR"
    }else {
-        ++result;
-        return "You win! "
+        currentResult.textContent = ++playerPoints
+        outcome.textContent = "You win!"
    } 
 }
-function score(points, result){
-    let score = 0;
-    return "Player Score: "+result+" COM Score: "+points
+const playerOutcome = document.querySelector('.playerOption')
+const computerOutcome = document.querySelector('.computerOption')
+function displayImg(){
+    
 }
 function getComputerChoice(){
     const optionsArray = ['Rock',  'Paper',  'Scissors'];
     return optionsArray[Math.floor(Math.random()*optionsArray.length)]
 }
-function game(){
-    for(let i = 0; i < 5; i++){
-        const computerSelection = getComputerChoice()
-        let playerSelection = prompt("You VS the COM, best of 5 wins!!", "");
-        playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
-        console.log("P1: "+playerSelection+ " VS COM: "+computerSelection);
-        console.log(playRound(playerSelection, computerSelection));
-        console.log(score(points, result))
-        if(!(playerSelection == 'Rock' || playerSelection == 'Paper' || playerSelection == 'Scissors')){
-        --i;
-        }else if(result == 3 && 3 >= i){
-           return console.log('You must be hacking!!')
-        }else if(points == 3 && 3 >= i){
-            return console.log('Wow you suck!!')
-        }
-    }
-    if(points < result){
-        console.log('You are the winner!!!!!!')
-    }else if(points > result){
-    console.log('You Lost! Try again!!!!!!')
-    }else if (result == 0 || points == 0){
-        alert('Enter Rock, Paper, or Scissors to play.')
-    }else{console.log('It\'s a tie!!!!')}
+function getPlayerChoice(){
+    if(choose === 'Rock'){
+        playerOutcome
+        return 'Rock'
+    }else if(choose === 'Paper'){
+        return 'Paper'
+    }else{return 'Scissors'}
 }
-console.log(game())
+
+function game(){
+    if(points === 5){
+        return "You lost against a Bot...that\'s sad."
+    }else{}
+}
