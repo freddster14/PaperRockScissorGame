30216e03f293908861215458089ea65b408476be
Zeronicc <zeronic4465@gmail.com>
2022-09-22 18:02:21 -0500

    DOM used to display results, when best of 5

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 const currentResult = document.getElementById('result');
 const currentPoints = document.getElementById('points');
 const outcome = document.querySelector('.textResult');
+const title = document.querySelector('.header');
+const body = document.querySelector('.page')
+
+const restart = document.querySelector('.resetBtn')
+restart.addEventListener('click', (e) => location.reload())
 
 const player = document.querySelector('.buttons');
-player.addEventListener('click', e => {playRound(); displayImg()});
-
+player.addEventListener('click', e => {playRound(); game(); displayImg(); removeBest()});
 
 const rockButton = document.querySelector('#rock');
 rockButton.addEventListener("click", (e) => choose = 'Rock');
@@ -18,8 +22,19 @@ scissorsButton.addEventListener("click", (e) => choose = 'Scissors');
 let points = 0;
 let playerPoints = 0;
 let choose = '';
-
-function playRound(playerSelection, computerSelection){ 
+let text = '';
+function getComputerChoice(text){
+    const optionsArray = ['Rock',  'Paper',  'Scissors'];
+    return optionsArray[Math.floor(Math.random()*optionsArray.length)]
+}
+console.log(text)
+function hideBody(){
+    let gameContainer = document.querySelector('.gameContainer');
+    gameContainer.style.opacity = 0;
+    gameContainer.style.transform = 'scale(0)';
+    gameContainer.style.display = 'none';
+}
+function playRound(){ 
     const computerChoice = getComputerChoice();
     const playerChoice = choose;
     if(playerChoice == 'Rock' && computerChoice == 'Paper') {
@@ -40,37 +55,115 @@ function playRound(playerSelection, computerSelection){
    }else {
         currentResult.textContent = ++playerPoints
         outcome.textContent = "You win!"
-   } 
+   }
 }
-const playerOutcome = document.querySelector('#playerOption')
 
+const info5 = document.querySelector(".info");
+function removeBest(){
+    info5.textContent = "";
+}
 const computerOutcome = document.querySelector('#computerOption')
-function displayImg(){
-    if(choose == 'Rock'){
+function displayComImg(){
+    if('Rock'){
         let img = document.createElement('img');
         img.src = "images/rock.png";
         img.id = "rock"
         playerOutcome.appendChild(img)
-    }else if(choose == 'Paper'){
+    }else if('Paper'){
         let img = document.createElement('img');
         img.src = "images/paper.png";
         img.id = "paper"
         playerOutcome.appendChild(img)
-    }else{
+    }else if ('Scissors'){
         let img = document.createElement('img');
         img.src = "images/scissors.png";
         img.id = "scissors"
         playerOutcome.appendChild(img)
     }
 }
-function getComputerChoice(){
-    const optionsArray = ['Rock',  'Paper',  'Scissors'];
-    return optionsArray[Math.floor(Math.random()*optionsArray.length)]
+const playerOutcome = document.querySelector('#playerOption')
+function displayImg(){
+    if(choose == 'Rock'){
+        let img = document.createElement('img');
+        img.src = "images/rock.png"; 
+        img.id = "rock";
+        playerOutcome.appendChild(img)
+    }else if(choose == 'Paper'){
+        let img = document.createElement('img');
+        img.src = "images/paper.png"; 
+        img.id = "paper"; 
+        playerOutcome.appendChild(img)
+    }else if(choose == 'Scissors'){
+        let img = document.createElement('img');
+        img.src = "images/scissors.png"; 
+        img.id = "scissors";
+        playerOutcome.appendChild(img)
+    }
 }
 
-
 function game(){
-    if(points === 5){
-        return "You lost against a Bot...that\'s sad."
-    }else{}
+    if(points === 5 && playerPoints === 5){
+        hideBody();
+        const score = document.createElement('div');
+        score.id = "scoreEnd";
+        const comScore = document.createElement('div');
+        comScore.id = "comScoreEnd";
+        score.textContent = `Player: ${playerPoints}`;
+        comScore.textContent = `Bot: ${points}`;
+        const endDiv = document.createElement('div');
+        endDiv.id = "end";
+        endDiv.textContent = "It's a draw! Try again.";
+        const startOver = document.createElement('button');
+        startOver.addEventListener('click', (e) => location.reload());
+        startOver.textContent = 'Try Again?';
+        const endContainer = document.createElement('div');
+        endContainer.id = "endContainer";
+        body.appendChild(endContainer)
+        endContainer.appendChild(score);
+        endContainer.appendChild(comScore)
+        body.appendChild(endDiv);
+        body.appendChild(startOver);
+    }else if(points === 5){
+        hideBody();
+        const score = document.createElement('div');
+        score.id = "scoreEnd";
+        const comScore = document.createElement('div');
+        comScore.id = "comScoreEnd";
+        score.textContent = `Player: ${playerPoints}`;
+        comScore.textContent = `Bot: ${points}`;
+        const endDiv = document.createElement('div');
+        endDiv.id = "end";
+        endDiv.textContent = "You lost against a Bot...that\'s sad.";
+        const startOver = document.createElement('button');
+        startOver.addEventListener('click', (e) => location.reload());
+        startOver.textContent = 'Try Again?';
+        const endContainer = document.createElement('div');
+        endContainer.id = "endContainer";
+        body.appendChild(endContainer)
+        endContainer.appendChild(score);
+        endContainer.appendChild(comScore)
+        body.appendChild(endDiv);
+        body.appendChild(startOver);
+    }else if(playerPoints === 5){
+        hideBody();
+        const score = document.createElement('div');
+        score.id = "scoreEnd";
+        const comScore = document.createElement('div');
+        comScore.id = "comScoreEnd";
+        score.textContent = `Player: ${playerPoints}`;
+        comScore.textContent = `Bot: ${points}`;
+        const endDiv = document.createElement('div');
+        endDiv.id = "end"
+        endDiv.textContent = "Wow you won! You just got lucky.";
+        const startOver = document.createElement('button');
+        startOver.addEventListener('click', (e) => location.reload());
+        startOver.textContent = 'Try Again?';
+        const endContainer = document.createElement('div');
+        endContainer.id = "endContainer";
+        body.appendChild(endContainer)
+        endContainer.appendChild(score);
+        endContainer.appendChild(comScore)
+        body.appendChild(endDiv);
+        body.appendChild(startOver);
+        }
 }
